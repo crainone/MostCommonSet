6f81a29668a458addb8d4ca7f7195e081a721d95
Caitlin Rainone <caitlin.rainone@gmail.co>
2017-01-24 07:20:36 -0800

    Started implementation of Most Common Set with the brute force approach: sort all the words, sort the list, and count. Signed-off-by: Caitlin Rainone <caitlin.rainone@gmail.co>

--- MostCommonSet/Program.cs
+++ MostCommonSet/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Diagnostics;
 
 namespace MostCommonSet
 {
@@ -27,6 +29,42 @@ namespace MostCommonSet
     {
         static void Main(string[] args)
         {
+            //Attempt 1: Brute force (use this as benchmark later)
+            FileStream fs = new FileStream("testdata.txt", FileMode.Open, FileAccess.Read);
+
+            //Load everything into memory
+            char[][] data = new char[20000][];
+            for (int j = 0; j < 20000; j++)
+            {
+                data[j] = new char[5];
+                for (int i=0; i<5; i++)
+                {
+                    data[j][i] = (char) fs.ReadByte();
+                }
+            }
+
+            for (int j = 0; j < 20000; j++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Debug.Write(data[j][i]);
+                }
+                Debug.WriteLine("");
+            }
+            Debug.Write("Never mind.");
+        }
+
+        /// <summary>
+        /// Given the entire chunk of data, rearranges the row specified by rowNumber to alphabetic order. 
+        /// </summary>
+        /// <remarks>
+        /// TODO: Is this pass-by-reference?
+        /// </remarks>
+        /// <param name="data">All input data, 20000 rows of 5 characters.</param>
+        /// <param name="rowNum">Which row to sort this time.</param>
+        private static void SortWord(char[,] data, int rowNum)
+        {
+            
         }
     }
 }

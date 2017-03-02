5010a1c52ec9fa87b7f348871d1e98c053f1b462
Caitlin Rainone <caitlin.rainone@gmail.co>
2017-03-01 22:07:33 -0800

    In case of long words, shouldn't try to sort the word. It counts cases of letters instead, and then sorts and dedupes the result. In progress.
    Signed-off-by: Caitlin Rainone <caitlin.rainone@gmail.co>

--- MostCommonSet/Program.cs
+++ MostCommonSet/Program.cs
@@ -20,7 +20,47 @@ namespace MostCommonSet
     /// this project is a success. Still, this should work as specified and be as idiot-proof as
     /// possible.
     /// </para>
-    /// TODO: Can I use a TODO tag?
+    /// <para>
+    /// Sample input:
+    /// <file>
+    /// abdnb
+    /// xnuch
+    /// pzhen
+    /// zphen
+    /// </file>
+    /// </para>
+    /// <para>
+    /// Representation in memory:
+    /// <table>
+    /// <tr><th>a</th><th>b</th><th>c</th><th>...</th><th>z</th></tr>
+    /// <tr><td>1</td><td>2</td><td>0</td><td></td><td>0</td></tr>
+    /// <tr><td>0</td><td>0</td><td>1</td><td></td><td>0</td></tr>
+    /// <tr><td>0</td><td>0</td><td>0</td><td></td><td>1</td></tr>
+    /// <tr><td>0</td><td>0</td><td>0</td><td></td><td>1</td></tr>
+    /// <tr></tr>
+    /// </table>
+    /// </para>
+    /// <para>
+    /// Then:
+    /// 0 -
+    ///   0 -
+    ///     0 -
+    ///       ...
+    ///         1 = 2 (from 000...1: "pzhen" and "zphen")
+    ///     1 -
+    ///       ...
+    ///         0 = 1 (from 001...0: "xnuch")
+    /// 1 -
+    ///   2 -
+    ///     0 -
+    ///       ...
+    ///         0 = 1 (from 120...0: "abdnb")
+    /// </para>
+    /// <para>
+    /// Result:
+    /// ehnpz
+    /// (since pzhen and zphen count as the same word, and appear collectively twice)
+    /// </para>
     /// TODO: Error handling
     /// TODO: Unit tests
     /// TODO: Optimize
@@ -29,15 +69,22 @@ namespace MostCommonSet
     {
         static void Main(string[] args)
         {
-            //Attempt 1: Brute force (use this as benchmark later)
+            //Attempt 2:
+            //Can take advantage of one thing, here: the number of letters per word is variable,
+            //and the number of words in the list is variable, but the number of letters is NOT.
+            //Represent each word with an array[26] of ints (nb should be shorts, not expected to
+            //have more than ~100 of any given letter). Expensive in memory but faster to process.
             FileStream fs = new FileStream("testdata.txt", FileMode.Open, FileAccess.Read);
 
             //Load everything into memory
-            char[][] data = new char[20000][];
+            int[][] data = new int[20000][];
             for (int j = 0; j < 20000; j++)
             {
-                //Each new word is an array of 5
-                data[j] = new char[5];
+                //Each new word is an array of 26
+                data[j] = new int[26];
+
+                //Note: Line breaks in the input file are mostly cosmetic, this will just read
+                //the next five characters regardless
                 for (int i=0; i<5; i++)
                 {
                     //Consume any whitespace or invalid characters
@@ -46,49 +93,20 @@ namespace MostCommonSet
                         inByte = fs.ReadByte();
                     while (inByte < 97 || inByte > 122);
 
-                    //Save valid characters to array
-                    //Note: Line breaks in the input file are mostly cosmetic, this will just read
-                    //the next five characters regardless
+                    //Save valid characters to array by incrementing at index a=0, b=1 ... z=25
                     char c = (char) inByte;
-                    data[j][i] = c;
+                    data[j][inByte-97]++;
                 }
             }
-
-            //Since we don't care about letter order, sort them
-            for (int j = 0; j < 20000; j++)
-            {
-                SortWord(data[j]);
-            }
-
-            //Don't need to actually sort, just count. Make a heap of the strings and increment when you reach 5 deep.
-            //Keep track of the highest value that turns up
-            int topCount = 0;
-            Heap newData = new Heap();
-            for(int i=0; i < 20000; i++)
-            {
-                int currentCount = newData.put(data[i]);
-                if(currentCount > topCount)
-                {
-                    topCount = currentCount;
-                } 
-            }
-
+            
+            //Sort the result
+            
+            //Now look for dupes now that anagrams are handled
+            
             //Traverse the heap and print out values that are topCount-ed
-            newData.printAll(topCount);
+            data.printAll(topCount);
 
             Debug.Write("Never mind."); 
-        }
-
-        /// <summary>
-        /// Sort the word in alphabetical order
-        /// </summary>
-        /// <remarks>
-        /// TODO: Is this pass-by-reference?
-        /// </remarks>
-        /// <param name="data">One rows of 5 characters from the input data.</param>
-        private static void SortWord(char[] data)
-        {
-            Array.Sort(data);
         }
 
         //This is the bottleneck remove ASAP
